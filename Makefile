a08eec2f289dfff504bbf6361dc9bf8214c7f91a
Igor <i.sulialev@mail.ru>
2022-04-24 14:49:57 +0300

    add brain-calc.js

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 instal:
 	npm ci
-brain-games:
-	node bin/brain-games.js
 publish:
 	npm publish --dry-run
 lint:
 	npx eslint .
+brain-games:
+	node bin/games/brain-games.js
+brain-even:
+	node bin/games/brain-even.js
+brain-calc:
+	node bin/games/brain-calc.js	
---
+++ bin/games/brain-calc.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import gameBody from '../../src/index.js';
+
+const gameQuestion = 'What is the result of the expression?';
+const getRandom = () => Math.floor(Math.random() * 30);
+const mathSymbols = ['+', '-', '*'];
+const getSolution = (a, symb, b) => {
+  switch (symb) {
+    case '+':
+      return (a + b);
+    case '-':
+      return (a - b);
+    case '*':
+      return (a * b);
+  }
+};
+
+const getTaskAndSol = () => {
+  const task = `${getRandom()} ${mathSymbols[Math.floor(Math.random() * mathSymbols.length)]} ${getRandom()}`;
+  const splitTask = task.split(' ');
+  const solution = getSolution(Number(splitTask[0]), splitTask[1], Number(splitTask[2]));
+  const taskAndSol = [];
+  taskAndSol.push(task);
+  taskAndSol.push(solution);
+  return taskAndSol;
+};
+
+gameBody(gameQuestion, getTaskAndSol);
