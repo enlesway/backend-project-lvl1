9475a122f5659bad9c297f1e348138db789bd1be
Igor <i.sulialev@mail.ru>
2022-04-25 22:45:25 +0300

    refactoring for no duplications

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -4,11 +4,10 @@ import gameBody from '../../src/index.js';
 
 const guide = 'Answer "yes" if the number is even, otherwise answer "no".';
 const getRandom = () => Math.round(Math.random() * 100);
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
 const getTaskAndSol = () => {
   const task = getRandom();
-  const solution = isEven(task);
+  const solution = (task % 2 === 0 ? 'yes' : 'no');
   const taskAndSol = [];
   taskAndSol.push(task);
   taskAndSol.push(solution);
