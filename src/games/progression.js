e8f67e7874f486b7745655f3e2f0cc67c2dc8db1
Igor <i.sulialev@mail.ru>
2022-05-01 21:58:34 +0300

    refactoring progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,18 +3,23 @@ import getRandom from '../random.js';
 
 const guide = 'What number is missing in the progression?';
 
-const generateRound = () => {
-  const progresStep = getRandom(5, 1);
-  let progresNum = getRandom(20, 0);
-  const taskArr = [];
-  for (let i = 0; i < 10; i += 1) {
-    taskArr.push(progresNum);
-    progresNum += progresStep;
+const getProgression = (progressionLength, progressionStep, progressionFirstNum) => {
+  const progressionArray = [];
+  for (let i = 0; i < progressionLength; i += 1) {
+    progressionArray.push(progressionFirstNum + (progressionStep * i));
   }
+  return progressionArray;
+};
+
+const generateRound = () => {
+  const progresLength = 10;
+  const progresStep = getRandom(5, 1);
+  const progresFirstNum = getRandom(20, 0);
+  const taskProgression = getProgression(progresLength, progresStep, progresFirstNum);
   const indexTaskNum = getRandom(10, 0);
-  const answer = taskArr[indexTaskNum];
-  taskArr[indexTaskNum] = '..';
-  const task = taskArr.join(' ');
+  const answer = taskProgression[indexTaskNum];
+  taskProgression[indexTaskNum] = '..';
+  const task = taskProgression.join(' ');
   return [task, answer];
 };
 
