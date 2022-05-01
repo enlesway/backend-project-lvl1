509e23dd5da6098261d8cf1ca7e3ccca3297a999
Igor <i.sulialev@mail.ru>
2022-05-01 21:58:22 +0300

    refactoring prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,14 +6,14 @@ const guide = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
   if (num < 4) return 'yes';
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) return 'no';
+    if (num % i === 0) return false;
   }
-  return 'yes';
+  return true;
 };
 
 const generateRound = () => {
-  const task = getRandom(1, 100);
-  const answer = isPrime(task);
+  const task = getRandom(100, 1);
+  const answer = isPrime(task) ? 'yes' : 'no';
   return [task, answer];
 };
 
