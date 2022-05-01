c043c1441b9d46b3b308e2ab11e818a76cbe4204
Igor <i.sulialev@mail.ru>
2022-05-01 21:20:31 +0300

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import readlineSync from 'readline-sync';
 
+const roundsAmount = 3;
+
 export default (gameGuide, roundGenerator) => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   console.log(gameGuide);
-
-  let roundCounter = 0;
-  let gameIn = true;
-
-  while (gameIn === true && roundCounter < 3) {
+  for (let i = 0; i < roundsAmount; i += 1) {
     const roundData = roundGenerator();
     const currentTask = roundData[0];
     const correctAns = String(roundData[1]);
     console.log(`Question: ${currentTask}`);
     const playerAns = readlineSync.question('Your answer: ');
-    if (correctAns === playerAns) {
-      console.log('Correct!');
-      roundCounter += 1;
-    } else {
-      console.log(`'${playerAns}' is wrong answer ;(. Correct answer was '${correctAns}'.\nLet's try again, ${playerName}!`);
-      gameIn = false;
+    if (correctAns !== playerAns) {
+      console.log(`'${playerAns}' is wrong answer ;(. Correct answer was '${correctAns}'`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
     }
+    console.log('Correct!');
   }
-  if (gameIn === true) console.log(`Congratulations, ${playerName}!`);
+  console.log(`Congratulations, ${playerName}!`);
 };
