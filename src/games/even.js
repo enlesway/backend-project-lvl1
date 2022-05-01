cf389cf2a0d934da1884e3f0086b0ade9a4ed94f
Igor <i.sulialev@mail.ru>
2022-05-01 21:58:07 +0300

    refactoring even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,11 @@ import getRandom from '../random.js';
 
 const guide = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => (num % 2 === 0);
 
 const generateRound = () => {
-  const task = getRandom(0, 100);
-  const answer = isEven(task);
+  const task = getRandom(100, 0);
+  const answer = isEven(task) ? 'yes' : 'no';
   return [task, answer];
 };
 
