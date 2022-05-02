95df359ec411491bf7404acd06b78575798ae552
Igor <i.sulialev@mail.ru>
2022-05-03 01:14:28 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,7 @@ const guide = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (num) => (num % 2 === 0);
 
 const generateRound = () => {
-  const task = getRandom(100, 0);
+  const task = getRandom(0, 100);
   const answer = isEven(task) ? 'yes' : 'no';
   return [task, answer];
 };
