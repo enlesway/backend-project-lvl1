3817d76ef11802114418eff045eb319cbcebe76f
Igor <i.sulialev@mail.ru>
2022-05-03 01:15:04 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,8 @@ import getRandom from '../random.js';
 const guide = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 4) return 'yes';
+  if (num < 2) return false;
+  if (num === 2) return true;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
@@ -12,7 +13,7 @@ const isPrime = (num) => {
 };
 
 const generateRound = () => {
-  const task = getRandom(100, 1);
+  const task = getRandom(1, 100);
   const answer = isPrime(task) ? 'yes' : 'no';
   return [task, answer];
 };
